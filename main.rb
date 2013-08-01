b6166bc2066070b25e10ffc13e2ee0709f70f3d4
Sergey-Russiyan <sergey.russiyan@gmail.com>
2013-08-01 22:53:14 +0300

    commit

--- main.rb
+++ main.rb
@@ -1,21 +1,16 @@
+
+
 # encoding: UTF-8
-# atrofymenko
-def is_integer(value)            
-  true if Integer(value) rescue false
-end
 
-def is_numeric(value)            
-  true if Float(value) rescue false
-end
-
-aa = nil
-loop do
-  begin
-    puts "Пожалуйста введите число:"
-  end until is_numeric(aa = gets.chomp)
-  break if is_integer(aa) 
-  print "Неверный формат. "
-end
-if aa.to_i.even? then puts "true"
-  else puts "false"
+puts "Enter any integer:"
+count = nil
+while count.to_i == 0 do
+  count = gets.chomp
+  if count.to_i == 0
+  puts "Не верный формат, пожалуйста введите число: "
+elsif count.to_i % 2 == 0
+  puts "true"
+else count.to_i % 2 == 1
+  puts "false"
+  end
 end
