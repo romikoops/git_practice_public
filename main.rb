182ae72c149a66f004866674a3bc39d7083c864b
trofymenko <oleksandr.trofymenko@gmail.com>
2013-08-01 22:08:27 +0300

    added sign to main.rb

--- main.rb
+++ main.rb
@@ -1,16 +1,21 @@
-#encoding UTF-8
+﻿# encoding: UTF-8
+# atrofymenko
+def is_integer(value)            
+  true if Integer(value) rescue false
+end
+
+def is_numeric(value)            
+  true if Float(value) rescue false
+end
+
+aa = nil
 loop do
-  puts 'Введите число'
-  a = gets.chomp
   begin
-    Integer(a)
-  rescue ArgumentError
-    puts 'Не верный формат, пожалуйста введите число:'
-  else
-    if (a.to_i % 2) == 0 then
-      puts 'true'
-    else
-      puts 'false'
-    end
-  end
+    puts "Пожалуйста введите число:"
+  end until is_numeric(aa = gets.chomp)
+  break if is_integer(aa) 
+  print "Неверный формат. "
+end
+if aa.to_i.even? then puts "true"
+  else puts "false"
 end
