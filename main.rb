b7eea8ba01060ab279819160f3445538278a0f41
Yuriy Kurenkov <yuramax@gmail.com>
2013-08-01 20:26:21 +0300

    Fixed content in main.rb file

--- main.rb
+++ main.rb
@@ -1,1 +1,15 @@
-puts 'Hello world!'
+# -*- encoding : utf-8 -*-
+
+puts "Введите любое число: "
+number = gets.chomp
+
+while number.match(/[^0-9]/)
+  puts "Не верный формат, пожалуйста, введите число: "
+  number = gets.chomp
+end
+
+if number.to_i%2 == 0
+  puts true
+else
+  puts false
+end                
