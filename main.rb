c8a320b61ca8dc3b42fd6fafad35395f5f142a73
alexspot <alex.kycherenko@gmail.com>
2013-08-01 20:19:47 +0300

    added lesson4 task1 source

--- main.rb
+++ main.rb
@@ -1,1 +1,16 @@
-puts 'Hello world!'
+#encoding UTF-8
+loop do
+  puts 'Введите число'
+  a = gets.chomp
+  begin
+    Integer(a)
+  rescue ArgumentError
+    puts 'Не верный формат, пожалуйста введите число:'
+  else
+    if (a.to_i % 2) == 0 then
+      puts 'true'
+    else
+      puts 'false'
+    end
+  end
+end
